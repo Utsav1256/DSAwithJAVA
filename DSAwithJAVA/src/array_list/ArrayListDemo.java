7489905c93a6678ce2507ab75962834dc7a43919
Utsav <utsavkumar5462@gmail.com>
2022-10-08 13:47:31 +0530

    create ArrayListDemo.java class in array_list package

---
+++ DSAwithJAVA/src/array_list/ArrayListDemo.java
@@ -0,0 +1,30 @@
+package array_list;
+
+import java.util.ArrayList;
+public class ArrayListDemo {
+	public static void main(String[] args) {
+//		To create an ArrayList
+		ArrayList <Integer> list1 = new ArrayList <> (); 
+		
+//		To check or print size of the ArrayList
+		System.out.println(list1.size());
+		
+//		To add element in the ArrayList
+		list1.add(15);
+		list1.add(20);
+		list1.add(25);
+		
+		System.out.println(list1.size());
+		
+//		To read values
+		System.out.println(list1.get(2));
+		
+//		Another option 
+		
+		
+		
+		
+		
+	}
+}
+
