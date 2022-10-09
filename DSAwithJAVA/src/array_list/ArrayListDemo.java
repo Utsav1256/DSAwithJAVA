586d73fbec7e581b1041e5453630976d390aa848
Utsav <utsavkumar5462@gmail.com>
2022-10-09 19:39:53 +0530

    Third commit

--- DSAwithJAVA/src/array_list/ArrayListDemo.java
+++ DSAwithJAVA/src/array_list/ArrayListDemo.java
@@ -58,6 +58,50 @@ public class ArrayListDemo {
 		for(int element: list1) {//for each integer element in list1
 			System.out.print(element + " ");
 		}
+		
+		System.out.println();
+		
+//		To create an ArrayList of String
+		ArrayList<String> list2 = new ArrayList <> ();
+		
+//		To check or print size of the ArrayList
+		System.out.println(list2.size());
+//		To add element in the ArrayList
+		list2.add("I");
+		list2.add("Myself.");
+		
+//		To read values
+		System.out.println(list2.get(0));
+		System.out.println(list2.get(1));
+		
+//		Another option to add elements in the ArrayList
+		//	list1.add(index, element);
+		list2.add(1, "Love");
+		
+//		To print all the elements of the ArrayList
+		for(int i = 0; i < list2.size(); i++) {
+			System.out.print(list2.get(i) + " ");
+		}
+		
+		System.out.println();
+		
+//		To remove element
+//		list2.remove(1);
+//		list2.remove(3);
+		
+//		To set element in the ArrayList
+		list2.add("I am");
+		list2.add("Ceo");
+		list2.set(4,"a Coder.");
+		
+		for(int i = 0; i < list2.size(); i++) {
+			if(i >= list2.size()-2) {
+				System.out.print(list2.get(i) + " ");
+			}
+		}
+		
+		
+		
 	}
 }
 
